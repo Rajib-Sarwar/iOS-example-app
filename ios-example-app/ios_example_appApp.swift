c7968bcb4c8b1e39a22461d66ecb13d5367b0fb0
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2026-02-09 17:34:56 -0500

    initial project

---
+++ ios-example-app/ios_example_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_example_appApp.swift
+//  ios-example-app
+//
+//  Created by Chowdhury Md Rajib Sarwar on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_example_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
